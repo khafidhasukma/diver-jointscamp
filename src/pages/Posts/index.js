c293ed5eecde35a52ec32308c1cb1ac9012d9c42
khafidhasukma@gmail.com <khafidhasukma@gmail.com>
2023-04-02 20:13:36 +0700

    [posts]: slicing design

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,8 +1,154 @@
+import { NavLink } from "react-router-dom";
+import about1 from "../../assets/images/about1.png";
+import arrow from "../../assets/images/icons/arrow.svg";
+import about2 from "../../assets/images/about2.png";
+
 const Post = () => {
     return (
-        <>
-            <h1 className="mt-24">Ini page Post</h1>
-        </>
+        <div className="container py-24">
+            <NavLink to="/post/1">
+                <div className="mb-6 grid grid-cols-2 items-center overflow-hidden rounded-2xl bg-white shadow-box">
+                    <img src={about2} className="h-full w-full  object-cover" alt="" />
+                    <div className="p-6">
+                        <p className="mb-3 text-sm font-semibold text-gray">27 Dec, 2022</p>
+                        <h1 className="mb-6 text-2xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                        <p className="text-gray1">
+                            Emily had always been fascinated by dolphins. When she heard about a place where she could
+                            dive with them, she knew she had to go lorem ipsum dolor sit When she heard about a place
+                            where she could dive with them, she knew she had to go lorem ipsum dolor ...
+                        </p>
+                        <div className="mt-4">
+                            <button className="flex items-center gap-2 text-primary">
+                                <p className="text-sm font-bold">Read More</p>
+                                <img src={arrow} width="20px" alt="Arrow" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </NavLink>
+            <div className="grid grid-cols-4 gap-4">
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+                <NavLink to="/posts/1" className="rounded-2xl bg-white p-3 shadow-box">
+                    <img src={about1} className="h-40 w-full rounded-2xl object-cover shadow-box" alt="" />
+                    <p className="mb-3 mt-2 text-end text-xs font-semibold text-gray">27 Dec, 2022</p>
+                    <h1 className="text-xl font-extrabold text-darkGray">Dive With Dolphins</h1>
+                    <p className="text-sm text-gray1">
+                        Emily had always been fascinated by dolphins. When she heard about a place where she could dive
+                        with them, she knew she had to go lorem ipsum dolor sit...
+                    </p>
+                    <div className="mt-4 flex justify-end">
+                        <button className="flex items-center gap-2 text-primary">
+                            <p className="text-sm font-bold">Read More</p>
+                            <img src={arrow} width="20px" alt="Arrow" />
+                        </button>
+                    </div>
+                </NavLink>
+            </div>
+        </div>
     );
 };
 
